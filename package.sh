930b2931282baf208daac355100acd7632c37dc7
Joris Quenee <joris.quenee@gmail.com>
2019-04-24 20:17:29 +0200

    add missing launcher

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 rm -f trap.opk
 rm -f src/*.pyc
 chmod +x src/main.py
-mksquashfs . trap.opk -all-root -noappend -no-exports -no-xattrs
+mksquashfs . trap.opk -all-root -noappend -no-exports -no-xattrs -e .gitignore deploy.sh package.sh .git/
